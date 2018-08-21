ef0a777ade6ebff5cabf44d748b2edea6d955ab3
Kathy Jang <kathyjang@berkeley.edu>
2018-08-20 17:38:26 -0700

    First iteration of environment is complete. This is the env used for UDSSC_Merge experiments 0-2

--- flow/envs/UDSSC_merge_env.py
+++ flow/envs/UDSSC_merge_env.py
@@ -72,8 +72,8 @@ class UDSSCMergeEnv(Env):
         self.n_following = env_params.additional_params["n_following"]
         self.n_merging_in = env_params.additional_params["n_merging_in"]
         self.n_obs_vehicles = \
-            1 + self.n_preceding + self.n_following + self.n_merging_in
-
+            1 + self.n_preceding + self.n_following + 2*self.n_merging_in
+        self.ring_radius = scenario.net_params.additional_params["ring_radius"]
         self.obs_var_labels = \
             ["speed", "pos", "queue_length", "velocity_stats"]
 
@@ -81,19 +81,11 @@ class UDSSCMergeEnv(Env):
 
     @property
     def observation_space(self):
-        # speed = Box(low=0, high=np.inf, shape=(self.n_obs_vehicles,),
-        #             dtype=np.float32)
-        # absolute_pos = Box(low=0., high=np.inf, shape=(self.n_obs_vehicles,),
-        #                    dtype=np.float32)
-        # queue_length = Box(low=0, high=np.inf, shape=(1,), dtype=np.float32)
-        # vel_stats = Box(low=-np.inf, high=np.inf, shape=(2,), dtype=np.float32)
-
-        # return Tuple((speed, absolute_pos, queue_length, vel_stats))
-        speed = Box(low=0, high=np.inf, shape=(self.n_obs_vehicles,),
-                    dtype=np.float32)
-        relative_pos = Box(low=0., high=np.inf, shape=(self.n_obs_vehicles,),
-                           dtype=np.float32) # Normalized by ring circumference 
-        return Tuple((speed, relative_pos))
+        box = Box(low=0.,
+                  high=1,
+                  shape=(self.n_obs_vehicles * 2,),
+                  dtype=np.float32)
+        return box
 
     @property
     def action_space(self):
@@ -130,107 +122,75 @@ class UDSSCMergeEnv(Env):
 
 
         """
-        ## changes start
-        # import ipdb; ipdb.set_trace()
+        try:
+            # import ipdb; ipdb.set_trace()
+            # Get normalization factors 
+            circ = float(2 * np.pi * self.ring_radius)
+            # max_speed = self.scenario.net_params.additional_params.get("speed_limit")
+            max_speed = self.scenario.max_speed 
+            merge_0_norm = self.scenario.edge_length('merge_in_0') + \
+                        self.scenario.edge_length('inflow_0')
+            merge_1_norm = self.scenario.edge_length('merge_in_1') + \
+                        self.scenario.edge_length('inflow_1') 
+            # RL POS AND VEL
+            rl_pos = [self.get_x_by_id('rl_0') / circ]
+            rl_vel = [self.vehicles.get_speed('rl_0') / max_speed]
 
-        # TODO: NORMALIZE EVERYTHING
-        rl_pos = self.get_x_by_id('rl_0')
-        rl_vel = self.vehicles.get_speed('rl_0')
-        close_0 = np.zeros(self.n_merging_in)
-        close_1 = np.zeros(self.n_merging_in)
+            # DISTANCES
+            merge_id_0, merge_id_1 = self.k_closest_to_merge(self.n_merging_in) # TODO check this is sorted
+            merge_dists_0 = self.process(self._dist_to_merge_0(merge_id_0),
+                                        length=self.n_merging_in,
+                                        normalizer=merge_0_norm) # pad and normalize
+            merge_dists_1 = self.process(self._dist_to_merge_1(merge_id_1),
+                                        length=self.n_merging_in,
+                                        normalizer=merge_1_norm)
+            # import ipdb; ipdb.set_trace()
 
-        merge_0, merge_1 = self.k_closest_to_merge(k)
-        tailway, headway = self.k_closest_to_rl('rl_0', k)
-        
-        velocities = self.vehicles.get_speed([merge_ids] + [close_ids])
-        rl_distances = [item[1] for item in [tailway] + [headway]]
-        merge_distances = self._dist_to_merge_0(merge_0) + 
-                          self._dist_to_merge_1(merge_1)
-        state = np.array([rl_pos, rl_vel, velocities,
-                          rl_distances, merge_distances])
+            # Get (ID, dist_from_RL) for the k vehicles closest to 
+            # the RL vehicle. 0 if there is no k_closest.
+            tailway, headway = self.k_closest_to_rl('rl_0', self.n_preceding) #todo
+            tailway_ids = [x[0] for x in tailway]
+            tailway_dists = [x[1] for x in tailway]
+            tailway_dists = self.process(tailway_dists,
+                                        length=self.n_preceding,
+                                        normalizer=circ)
+
+            headway_ids = [x[0] for x in headway]
+            headway_dists = [x[1] for x in headway]
+            headway_dists = self.process(headway_dists,
+                                        length=self.n_preceding,
+                                        normalizer=circ)
+
+
+            # VELOCITIES
+            merge_0_vel = self.process(self.vehicles.get_speed(merge_id_0),
+                                    length=self.n_merging_in,
+                                    normalizer=max_speed)
+            merge_1_vel = self.process(self.vehicles.get_speed(merge_id_1),
+                                    length=self.n_merging_in,
+                                    normalizer=max_speed)
+            tailway_vel = self.process(self.vehicles.get_speed(tailway_ids),
+                                    length=self.n_preceding,
+                                    normalizer=max_speed)
+            headway_vel = self.process(self.vehicles.get_speed(headway_ids),
+                                    length=self.n_following,
+                                    normalizer=max_speed)
+            # return np.array(np.concatenate([speeds, dist_to_intersec, edge_number,
+            #                                 density, velocity_avg,
+            #                                 self.last_change.flatten().tolist()]))
+            # return np.array([normalized_vel, normalized_pos, queue_length,
+            #                  vel_stats]).T
+            # state = np.array([rl_pos, ])
+            state = np.array(np.concatenate([rl_pos, rl_vel,
+                                            merge_dists_0, merge_0_vel,
+                                            merge_dists_1, merge_1_vel,
+                                            tailway_dists, tailway_vel,
+                                            headway_dists, headway_vel]))
+                                            
+            # import ipdb; ipdb.set_trace()
+        except:
+            import ipdb; ipdb.set_trace()
         return state
-        
-        # Calculate distances. 
-        
-
-
-        ## changes end
-
-        ### OLD STUFF STARTS
-        # sorted_data = self.sorted_extra_data
-        # merge_len = self.scenario.intersection_length
-
-        # # Merge stretch is pos 0.0-25.5 (ish), so actively merging vehicles
-        # # are sorted at the front of the list. Otherwise, vehicles closest to
-        # # the merge are at the end of the list (effectively reverse sorted).
-        # if self.get_x_by_id(sorted_data[0]) < merge_len and self.get_x_by_id(
-        #         sorted_data[1]) < merge_len:
-        #     if not sorted_data[0].startswith("merge") and \
-        #             not sorted_data[1].startswith("merge"):
-        #         vid1 = sorted_data[-1]
-        #         vid2 = sorted_data[-2]
-        #     elif not sorted_data[0].startswith("merge"):
-        #         vid1 = sorted_data[1]
-        #         vid2 = sorted_data[-1]
-        #     elif not sorted_data[1].startswith("merge"):
-        #         vid1 = sorted_data[0]
-        #         vid2 = sorted_data[-1]
-        #     else:
-        #         vid1 = sorted_data[1]
-        #         vid2 = sorted_data[0]
-        # elif self.get_x_by_id(sorted_data[0]) < merge_len:
-        #     vid1 = sorted_data[0]
-        #     vid2 = sorted_data[-1]
-        # else:
-        #     vid1 = sorted_data[-1]
-        #     vid2 = sorted_data[-2]
-        # pos[-2] = self.get_x_by_id(vid1)
-        # pos[-1] = self.get_x_by_id(vid2)
-        # vel[-2] = self.vehicles.get_speed(vid1)
-        # vel[-1] = self.vehicles.get_speed(vid2)
-
-        # # find and eliminate all the vehicles on the outer ring
-        # num_inner = len(sorted_data)
-
-        # rl_vehID = self.vehicles.get_rl_ids()[0]
-        # rl_srtID, = np.where(sorted_data == rl_vehID)
-        # rl_srtID = rl_srtID[0]
-
-        # # FIXME(cathywu) hardcoded for self.num_preceding = 2
-        # lead_id1 = sorted_data[(rl_srtID + 1) % num_inner]
-        # lead_id2 = sorted_data[(rl_srtID + 2) % num_inner]
-        # # FIXME(cathywu) hardcoded for self.num_following = 2
-        # follow_id1 = sorted_data[(rl_srtID - 1) % num_inner]
-        # follow_id2 = sorted_data[(rl_srtID - 2) % num_inner]
-        # vehicles = [rl_vehID[0], lead_id1, lead_id2, follow_id1, follow_id2]
-
-        # vel[:self.n_obs_vehicles - self.n_merging_in] = np.array(
-        #     self.vehicles.get_speed(vehicles))
-        # pos[:self.n_obs_vehicles - self.n_merging_in] = np.array(
-        #     [self.get_x_by_id(veh_id) for veh_id in vehicles])
-
-        # # normalize the speed
-        # # FIXME(cathywu) can divide by self.max_speed
-        # normalized_vel = np.array(vel) / self.scenario.max_speed
-
-        # # normalize the position
-        # normalized_pos = np.array(pos) / self.scenario.length
-
-        # # Compute number of vehicles in the outer ring
-        # queue_length = np.zeros(1)
-        # queue_length[0] = len(sorted_data) - num_inner
-
-        # # Compute mean velocity on inner and outer rings
-        # # Note: merging vehicles count towards the inner ring stats
-        # vel_stats = np.zeros(2)
-        # vel_all = self.vehicles.get_speed(sorted_data)
-        # vel_stats[0] = np.mean(vel_all[:num_inner])
-        # vel_stats[1] = np.mean(vel_all[num_inner:])
-        # vel_stats = np.nan_to_num(vel_stats)
-
-        # return np.array([normalized_vel, normalized_pos, queue_length,
-        #                  vel_stats]).T
-        ### OLD STUFF ENDS 
 
     def sort_by_position(self):
         """
@@ -256,38 +216,47 @@ class UDSSCMergeEnv(Env):
 
 
     def k_closest_to_merge(self, k):
-        close_0 = np.zeros(self.n_merging_in)
-        close_1 = np.zeros(self.n_merging_in)
+        """
+        Return a list of IDs, NO DISTANCES. NO TRUNCATION.
 
-        # both dists_0 and dists_1 increase the closer you get to the intersection
-        # dists_0 = self.vehicles.get_x_by_id(self.vehicles.get_ids_by_edge(["merge_in_0", "inflow_0"]))
-        # dists_1 = self.vehicles.get_x_by_id(self.vehicles.get_ids_by_edge(["merge_in_1", "inflow_1"]))
+        [veh_id, veh_id, veh_id] 
+        """
 
-        # total_len = self.scenario.edge_length("merge_in_0") +
-        #             self.scenario.edge_length("inflow_0")
+        # close_0 = []
 
-        # return self.total_edgestarts_dict[edge] + position
-
-
-        dists_0 = sorted(self.vehicles.get_ids_by_edge(["merge_in_0", "inflow_0"]), 
+        close_0 = sorted(self.vehicles.get_ids_by_edge(["merge_in_0", "inflow_0"]), 
                          key=lambda veh_id:
-                         -self.vehicles.get_x_by_id(veh_id))
+                         -self.get_x_by_id(veh_id))
 
-        dists_1 = sorted(self.vehicles.get_ids_by_edge(["merge_in_1", "inflow_1"]), 
+        close_1 = sorted(self.vehicles.get_ids_by_edge(["merge_in_1", "inflow_1"]), 
                          key=lambda veh_id:
-                         -self.vehicles.get_x_by_id(veh_id))
+                         -self.get_x_by_id(veh_id))
 
-        for i in range(min(k, len(dists_0))):
-            close_0[i] = dists_0
+        if len(close_0) > k:
+            close_0 = close_0[:k]
 
-        for i in range(min(k, len(dists_1))):
-            close_1[i] = dists_1
+        if len(close_1) > k:
+            close_1 = close_1[:k]
+
+        # for i in range(min(k, len(dists_0))):
+        #     close_0[i] = dists_0
+
+        # for i in range(min(k, len(dists_1))):
+        #     close_1[i] = dists_1
         
         return close_0, close_1
 
     def k_closest_to_rl(self, rl_id, k):
         """
         Return a list of ids and  distances to said rl vehicles
+
+        In the form:
+        [(veh_id, dist), (veh_id, dist)]
+
+        TODO: Is padding necessary in this step? I'm leaning toward not.
+
+        ASSUME NO TRUNCATION. PAD WITH ZEROS IN GET_STATE
+
         """
 
         # Arrays holding veh_ids of vehicles before/after the rl_id.
@@ -296,73 +265,84 @@ class UDSSCMergeEnv(Env):
         k_headway = []
 
         # Prep.
+        # try:
         route = ["top", "left", "bottom", "right"]
+        route = ["top", ":c_2", "left", ":d_2", "bottom", ":a_2", "right", ":b_2"]
         rl_edge = self.vehicles.get_edge(rl_id)
+        if rl_edge == "":
+            return [], []
         rl_index = route.index(rl_edge) 
         rl_x = self.get_x_by_id(rl_id)
-        rl_pos = self.scenario.get_position(rl_id)
+        rl_pos = self.vehicles.get_position(rl_id)
+        # except:
+        #     import ipdb; ipdb.set_trace()
 
         # Get preceding first.
         for i in range(rl_index, rl_index-2, -1): # Curr  edge and preceding edge
             if i == rl_index: # Same edge as rl_id
-                veh_ids = [(v, rl_pos - self.scenario.get_position(v)) 
+                veh_ids = [(v, rl_pos - self.vehicles.get_position(v)) 
                            for v in self.vehicles.get_ids_by_edge(route[i]) 
-                           if self.scenario.get_position(v) < rl_pos]
+                           if self.vehicles.get_position(v) < rl_pos]
             else: # Preceding edge 
                 edge_len = self.scenario.edge_length(route[i])
                 veh_ids = [(v, rl_pos + (edge_len - self.vehicles.get_position(v))) 
                            for v in self.vehicles.get_ids_by_edge(route[i])]
-            sorted_vehs = sorted(veh_ids, lambda v: self.get_x_by_id(v))
+            sorted_vehs = sorted(veh_ids, key=lambda v: self.get_x_by_id(v[0]))
             # k_tailway holds veh_ids in decreasing order of closeness 
             k_tailway = sorted_vehs + k_tailway
 
         # Get headways second.
         for i in range(rl_index, rl_index+2):
+            i = i % len(route)
             # If statement is to cover the case of overflow in get_x 
             if i == rl_index: # Same edge as rl_id
-                veh_ids = [(v, self.scenario.get_position(v) - rl_pos) 
+                veh_ids = [(v, self.vehicles.get_position(v) - rl_pos) 
                            for v in self.vehicles.get_ids_by_edge(route[i]) 
-                           if self.scenario.get_position(v) > rl_pos]
+                           if self.vehicles.get_position(v) > rl_pos]
             else:
-                rl_dist = self.scenario.edge_length(rl_edge) 
-                          - self.scenario.get_position(rl_id)
+                rl_dist = self.scenario.edge_length(rl_edge) - \
+                          self.vehicles.get_position(rl_id)
                 veh_ids = [(v, self.vehicles.get_position(v) + rl_dist)
-                           for self.vehicles.get_ids_by_edge(route[i])]
+                           for v in self.vehicles.get_ids_by_edge(route[i])]
             # The following statement is safe because it's being done
             # by one edge at a time
-            sorted_vehs = sorted(veh_ids, lambda v: self.get_x_by_id(v))
+            sorted_vehs = sorted(veh_ids, key=lambda v: self.get_x_by_id(v[0]))
             k_headway += sorted_vehs
 
         # After this step, truncate if necessary, or pad with zeros.
-        if len(k_tailway) > k:
-            k_tailway = k[-k:]
-        else:
-            k_tailway = [0] * (k - len(k_tailway))
+        # if len(k_tailway) > k:
+        #     k_tailway = k[-k:]
                 
-        if len(k_headway) > k:
-            k_headway = k[:k]
-        else: 
-            k_headway += [0] * (k - len(k_headway))
+        # if len(k_headway) > k:
+        #     k_headway = k[:k]
 
-        return k_tailway, k_headway
+        return k_tailway[::-1], k_headway
 
     def _dist_to_merge_1(self, veh_id):
         # distances = []
-        reference = self.scenario.total_edgestarts_dict["merge_in_1"] +
+        reference = self.scenario.total_edgestarts_dict["merge_in_1"] + \
                     self.scenario.edge_length("merge_in_1")
-        distances = [reference - self.vehicles.get_x_by_id(v)
+        distances = [reference - self.get_x_by_id(v)
                      for v in veh_id]
         return distances
 
     def _dist_to_merge_0(self, veh_id):
-        reference = self.scenario.total_edgestarts_dict["merge_in_0"] +
+        reference = self.scenario.total_edgestarts_dict["merge_in_0"] + \
                     self.scenario.edge_length("merge_in_0")
-        distances = [reference - self.vehicles.get_x_by_id(v)
+        distances = [reference - self.get_x_by_id(v)
                      for v in veh_id]
         return distances
 
-    def _dist_to_rl(self, rl_id, veh_id):
-        rl_edge = self.vehicles.get_edge(rl_id)
-        route = 
-        distances = 
-        for veh_
+    def process(self, state, length=None, normalizer=1):
+        """
+        Takes in a list, returns a normalized version of the list
+        with padded zeros at the end 
+        """
+        if length: # Truncation or padding
+            if len(state) < length:
+                state += [0.] * (length - len(state))
+            else:
+                state = state[:length]
+        state = [x / normalizer for x in state]
+        return state
+        
