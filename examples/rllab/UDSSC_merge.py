13888c4351d7fc554ed5e33dc3d7d2681fc26430
Kathy Jang <kathyjang@berkeley.edu>
2018-09-20 15:35:07 -0700

    roundabout_a21_2018_09_20_15_32_13

--- examples/rllab/UDSSC_merge.py
+++ examples/rllab/UDSSC_merge.py
@@ -26,8 +26,8 @@ from flow.core.params import InFlows
 HORIZON = 500
 SIM_STEP = 1
 BATCH_SIZE = 20000
-ITR = 200
-exp_tag = "roundabout_a20"  # experiment prefix
+ITR = 100
+exp_tag = "roundabout_a21"  # experiment prefix
 
 # Sumo settings
 FLOW_RATE = 350
@@ -84,23 +84,24 @@ def run_task(*_):
     # # -->
 
     inflow = InFlows()
-    # inflow.add(veh_type="rl", edge="inflow_0", name="rl", vehs_per_hour=50)
-    inflow.add(veh_type="rl", edge="inflow_0", name="rl", probability=50/3600)
-    # inflow.add(veh_type="idm", edge="inflow_0", name="idm", vehs_per_hour=50)
-    # inflow.add(veh_type="idm", edge="inflow_0", name="idm", vehs_per_hour=50)
-    inflow.add(veh_type="idm", edge="inflow_0", name="idm", probability=50/3600)
-    inflow.add(veh_type="idm", edge="inflow_0", name="idm", probability=50/3600)
+    inflow.add(veh_type="rl", edge="inflow_0", name="rl", vehs_per_hour=50)
+    # inflow.add(veh_type="rl", edge="inflow_0", name="rl", probability=50/3600)
+    inflow.add(veh_type="idm", edge="inflow_0", name="idm", vehs_per_hour=50)
+    inflow.add(veh_type="idm", edge="inflow_0", name="idm", vehs_per_hour=50)
+    # inflow.add(veh_type="idm", edge="inflow_0", name="idm", probability=50/3600)
+    # inflow.add(veh_type="idm", edge="inflow_0", name="idm", probability=50/3600)
     
     # inflow.add(veh_type="idm", edge="inflow_1", name="idm", vehs_per_hour=50)
     # inflow.add(veh_type="idm", edge="inflow_1", name="idm", vehs_per_hour=50)
     # inflow.add(veh_type="idm", edge="inflow_1", name="idm", vehs_per_hour=50)
     # inflow.add(veh_type="idm", edge="inflow_1", name="idm", vehs_per_hour=50)
     # inflow.add(veh_type="idm", edge="inflow_1", name="idm", vehs_per_hour=50)
-    inflow.add(veh_type="idm", edge="inflow_1", name="idm", probability=50/3600)
-    inflow.add(veh_type="idm", edge="inflow_1", name="idm", probability=50/3600)
-    inflow.add(veh_type="idm", edge="inflow_1", name="idm", probability=50/3600)
-    inflow.add(veh_type="idm", edge="inflow_1", name="idm", probability=50/3600)
-    inflow.add(veh_type="idm", edge="inflow_1", name="idm", probability=50/3600)
+    # inflow.add(veh_type="idm", edge="inflow_1", name="idm", probability=50/3600)
+    # inflow.add(veh_type="idm", edge="inflow_1", name="idm", probability=50/3600)
+    # inflow.add(veh_type="idm", edge="inflow_1", name="idm", probability=50/3600)
+    # inflow.add(veh_type="idm", edge="inflow_1", name="idm", probability=50/3600)
+    # inflow.add(veh_type="idm", edge="inflow_1", name="idm", probability=50/3600)
+    inflow.add(veh_type="idm", edge="inflow_1", name="idm", probability=300/3600)
     # note that the vehicles are added sequentially by the generator,
     # so place the merging vehicles after the vehicles in the ring
     vehicles = Vehicles()
