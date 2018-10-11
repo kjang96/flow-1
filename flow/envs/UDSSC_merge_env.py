4402c19e82e83b6764e4f7a4109617d6dca22646
Kathy Jang <kathyjang@berkeley.edu>
2018-10-10 22:14:59 -0700

    complex_0

--- flow/envs/UDSSC_merge_env.py
+++ flow/envs/UDSSC_merge_env.py
@@ -167,9 +167,9 @@ class UDSSCMergeEnv(Env):
         # headway_reward = 0.2 * max_cost * rewards.penalize_headway_variance(
             # self.vehicles, self.sorted_extra_data, normalization)
         # return vel_reward + headway_reward
-        # return vel_reward
+        return vel_reward
         # return total_vel
-        return avg_vel_reward + penalty
+        # return avg_vel_reward + penalty
 
     def get_state(self, **kwargs):
         """
