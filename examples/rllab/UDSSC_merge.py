69cf7b3d45b2aeb3b4e60c46af88b2bde2be48cb
Kathy Jang <kathyjang@berkeley.edu>
2018-10-12 14:13:09 -0700

    roundabout_59

--- examples/rllab/UDSSC_merge.py
+++ examples/rllab/UDSSC_merge.py
@@ -23,11 +23,11 @@ from flow.scenarios.UDSSC_merge.scenario import UDSSCMergingScenario
 from flow.core.params import InFlows
 
 # Training settings
-HORIZON = 500
-SIM_STEP = 1
+HORIZON = 5000
+SIM_STEP = 0.1
 BATCH_SIZE = 20000
 ITR = 150
-exp_tag = "roundabout_58"  # experiment prefix
+exp_tag = "roundabout_59"  # experiment prefix
 
 # Sumo settings
 FLOW_RATE = 350
@@ -91,7 +91,7 @@ def run_task(*_):
 
     inflow = InFlows()
     
-    inflow.add(veh_type="idm", edge="inflow_0", name="idm", probability=100/3600)
+    inflow.add(veh_type="idm", edge="inflow_0", name="idm", probability=50/3600)
     inflow.add(veh_type="rl", edge="inflow_0", name="rl", vehs_per_hour=50)
     # inflow.add(veh_type="rl", edge="inflow_0", name="rl", probability=50/3600)
 
