d171a44fcf8f57d06f30566df90ca480f7811359
Kathy Jang <kathyjang@berkeley.edu>
2018-10-15 12:15:38 -0700

    complex_18

--- examples/rllab/roundabout.py
+++ examples/rllab/roundabout.py
@@ -29,7 +29,7 @@ HORIZON = 500
 SIM_STEP = 1
 BATCH_SIZE = 20000
 ITR = 600
-exp_tag = "complex_17"  # experiment prefix
+exp_tag = "complex_18"  # experiment prefix
 
 # Sumo settings
 FLOW_RATE = 350
@@ -39,12 +39,12 @@ FLOW_PROB = FLOW_RATE/3600
 RL_FLOW_RATE = 50
 RL_FLOW_PROB = RL_FLOW_RATE/3600
 
-# # Local settings
-# N_PARALLEL = 1
-# SUMO_BINARY = "sumo-gui"
-# MODE = "local"
-# RESTART_INSTANCE = False
-# SEEDS = [1]
+# Local settings
+N_PARALLEL = 1
+SUMO_BINARY = "sumo"
+MODE = "local"
+RESTART_INSTANCE = False
+SEEDS = [1]
 
 # # EC2 settings
 # N_PARALLEL = 8
@@ -54,11 +54,11 @@ RL_FLOW_PROB = RL_FLOW_RATE/3600
 # SEEDS = [1, 2, 5, 91]
 
 # Autoscaler settings
-N_PARALLEL = 8
+N_PARALLEL = 40
 SUMO_BINARY = "sumo"
 MODE = "local"
 RESTART_INSTANCE = True
-SEEDS = [1, 2, 5, 91]
+SEEDS = [1, 2, 5, 7, 91, 104]
 
 
 def main():
@@ -94,24 +94,9 @@ def run_task(*_):
 
     inflow = InFlows()
     inflow.add(veh_type="rl", edge="inflow_0", name="rl", vehs_per_hour=50, departLane="random")
-    # inflow.add(veh_type="rl", edge="inflow_0", name="rl", probability=50/3600)
     inflow.add(veh_type="idm", edge="inflow_0", name="idm", vehs_per_hour=50, departLane="random")
     inflow.add(veh_type="idm", edge="inflow_0", name="idm", vehs_per_hour=50, departLane="random")
     inflow.add(veh_type="idm", edge="inflow_0", name="idm", vehs_per_hour=50, departLane="random")
-    # inflow.add(veh_type="idm", edge="inflow_0", name="idm", vehs_per_hour=50, departLane="random")
-    # inflow.add(veh_type="idm", edge="inflow_0", name="idm", probability=50/3600)
-    # inflow.add(veh_type="idm", edge="inflow_0", name="idm", probability=50/3600)
-    
-    # inflow.add(veh_type="idm", edge="inflow_1", name="idm", vehs_per_hour=50)
-    # inflow.add(veh_type="idm", edge="inflow_1", name="idm", vehs_per_hour=50)
-    # inflow.add(veh_type="idm", edge="inflow_1", name="idm", vehs_per_hour=50)
-    # inflow.add(veh_type="idm", edge="inflow_1", name="idm", vehs_per_hour=50)
-    # inflow.add(veh_type="idm", edge="inflow_1", name="idm", vehs_per_hour=50)
-    # inflow.add(veh_type="idm", edge="inflow_1", name="idm", probability=50/3600)
-    # inflow.add(veh_type="idm", edge="inflow_1", name="idm", probability=50/3600)
-    # inflow.add(veh_type="idm", edge="inflow_1", name="idm", probability=50/3600)
-    # inflow.add(veh_type="idm", edge="inflow_1", name="idm", probability=50/3600)
-    # inflow.add(veh_type="idm", edge="inflow_1", name="idm", probability=50/3600)
     
     inflow.add(veh_type="rl", edge="inflow_1", name="rl", vehs_per_hour=50, departLane="random")
     inflow.add(veh_type="idm", edge="inflow_1", name="idm", probability=500/3600, departLane="random")
