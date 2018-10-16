2e2f54aed95e5a073074009fa1fdcc32ba0ed914
Kathy Jang <kathyjang@berkeley.edu>
2018-10-16 10:46:07 -0700

    new_7

--- examples/rllab/UDSSC_merge.py
+++ examples/rllab/UDSSC_merge.py
@@ -24,11 +24,11 @@ from flow.scenarios.UDSSC_merge.scenario import UDSSCMergingScenario
 from flow.core.params import InFlows
 
 # Training settings
-HORIZON = 500
-SIM_STEP = 1
+HORIZON = 5000
+SIM_STEP = 0.1
 BATCH_SIZE = 20000
-ITR = 100
-exp_tag = "new_6"  # experiment prefix
+ITR = 60
+exp_tag = "new_7"  # experiment prefix
 
 # Sumo settings
 FLOW_RATE = 350
@@ -38,12 +38,12 @@ FLOW_PROB = FLOW_RATE/3600
 RL_FLOW_RATE = 50
 RL_FLOW_PROB = RL_FLOW_RATE/3600
 
-# Local settings
-N_PARALLEL = 1
-SUMO_BINARY = "sumo-gui" 
-MODE = "local"
-RESTART_INSTANCE = False
-SEEDS = [1]
+# # Local settings
+# N_PARALLEL = 1
+# SUMO_BINARY = "sumo-gui" 
+# MODE = "local"
+# RESTART_INSTANCE = False
+# SEEDS = [1]
 
 # # EC2 settings
 # N_PARALLEL = 8
@@ -52,12 +52,12 @@ SEEDS = [1]
 # RESTART_INSTANCE = True
 # SEEDS = [1, 2, 5, 91]
 
-# # Autoscaler settings
-# N_PARALLEL = 20
-# SUMO_BINARY = "sumo"
-# MODE = "local"
-# RESTART_INSTANCE = True
-# SEEDS = [1, 2, 5, 91]
+# Autoscaler settings
+N_PARALLEL = 20
+SUMO_BINARY = "sumo"
+MODE = "local"
+RESTART_INSTANCE = True
+SEEDS = [1, 2, 5, 91]
 
 def main():
     for seed in SEEDS:
