badcee8ff9b3710bfa7cd17a0e28c5649fe3939a
Kathy Jang <kathyjang@berkeley.edu>
2018-11-28 20:04:16 -0800

    ma_21

--- examples/rllib/multiagent_UDSSC_merge.py
+++ examples/rllib/multiagent_UDSSC_merge.py
@@ -35,7 +35,7 @@ HORIZON = 500
 SIM_STEP = 1
 ITR = 100
 N_ROLLOUTS = 40
-exp_tag = "ma_20"  # experiment prefix
+exp_tag = "ma_21"  # experiment prefix
 
 # # Local settings
 # N_CPUS = 1
@@ -103,7 +103,7 @@ flow_params = dict(
     exp_tag=exp_tag,
 
     # name of the flow environment the experiment is running on
-    env_name='MultiAgentUDSSCMergeEnv',
+    env_name='MultiAgentUDSSCMergeEnvReset',
 
     # name of the scenario class the experiment is running on
     scenario='UDSSCMergingScenario',
@@ -135,9 +135,9 @@ flow_params = dict(
             # number of observable merging-in vehicle from the larger loop
             "n_merging_in": 6,
             # rl action noise
-            "rl_action_noise": 0.5,
+            # "rl_action_noise": 0.5,
             # noise to add to the state space
-            "state_noise": 0.1,
+            # "state_noise": 0.1,
             # what portion of the ramp the RL vehicle isn't controlled for 
             # "control_length": 0.1,
             'perturb_weight': 0.03
