47a7418723a7831974bd3e0c916fcb9a1655eacd
Kathy Jang <kathyjang@berkeley.edu>
2018-12-07 19:01:16 -0800

    kathy_ma_tune_1

--- examples/rllib/multiagent_UDSSC_merge.py
+++ examples/rllib/multiagent_UDSSC_merge.py
@@ -35,7 +35,7 @@ HORIZON = 500
 SIM_STEP = 1
 ITR = 100
 N_ROLLOUTS = 40
-exp_tag = "kathy_ma_tune"  # experiment prefix
+exp_tag = "kathy_ma_tune_1"  # experiment prefix
 LOCAL = False
 
 # # # Local settings
