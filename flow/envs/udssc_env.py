e6fe209642025f46155a5e0ab37b6d0606216bec
Kathy Jang <kathyjang@berkeley.edu>
2019-08-28 16:13:34 -0700

    icra_33

--- flow/envs/udssc_env.py
+++ flow/envs/udssc_env.py
@@ -196,7 +196,8 @@ class UDSSCMergeEnv(Env):
 
         # print('avg_vel: %.2f, min_delay: %.2f, penalty: %.2f, penalty_2: %.2f, penalty_jerk: %.2f, penalty_speed: %.2f' % \
         #       (avg_vel, min_delay, penalty, penalty_2, penalty_jerk, penalty_speeding))
-        return 2 * min_delay + penalty + penalty_2 + penalty_jerk + penalty_speeding
+        # return 2 * min_delay + penalty + penalty_2 + penalty_jerk + penalty_speeding
+        return min_delay + penalty_jerk + penalty_speeding 
 
     def get_state(self, **kwargs):
         """
