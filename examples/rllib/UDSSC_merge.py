bfad199b690c1476908991211d42f483f395d1a8
Kathy Jang <kathyjang@berkeley.edu>
2019-08-30 21:14:40 -0700

    icra_58

--- examples/rllib/UDSSC_merge.py
+++ examples/rllib/UDSSC_merge.py
@@ -24,7 +24,7 @@ HORIZON = 500
 SIM_STEP = 1
 ITR = 160
 N_ROLLOUTS = 40
-exp_tag = "icra_57"  # experiment prefix
+exp_tag = "icra_58"  # experiment prefix
 
 # # Local settings
 # N_CPUS = 1
@@ -131,15 +131,15 @@ flow_params = dict(
             # batch size, for use in UDSSCMergeEnvReset
             "batch_size": HORIZON * N_ROLLOUTS,
             # # rl action noise
-            # "rl_action_noise": 0.2,
+            "rl_action_noise": 0.2,
             # # noise to add to the state space
-            "state_noise": 0.1,
+            # "state_noise": 0.1,
             # to be used with rl_action_noise; overrides that value 
-            "merge_norm_noise": 0.05,
+            # "merge_norm_noise": 0.05,
             # to be used with rl_action_noise; overrides that value 
-            "scenario_length_noise": 0.02,
+            # "scenario_length_noise": 0.02,
             # to be used with Reset env
-            "no_inflow_noise": True,
+            # "no_inflow_noise": True,
             # what portion of the ramp the RL vehicle isn't controlled for 
             "control_length": 0.1,
             # range of inflow lengths for inflow_0, inclusive
