91474b77797367c7e01047446586219708d0def1
Kathy Jang <kathyjang@berkeley.edu>
2020-03-27 17:00:07 -0700

    kjtro25

--- flow/multiagent_envs/multiagent_env.py
+++ flow/multiagent_envs/multiagent_env.py
@@ -123,7 +123,7 @@ class MultiEnv(MultiAgentEnv, Env):
 
         for rl_id in self.k.vehicle.get_arrived_rl_ids():
             done[rl_id] = True
-            reward[rl_id] = 100
+            reward[rl_id] = 500
             states[rl_id] = np.zeros(self.observation_space.shape[0])
             
         return states, reward, done, infos
